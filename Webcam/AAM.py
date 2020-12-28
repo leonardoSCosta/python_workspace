62b87a9336e04c9e9780d059211bc869824b36bf
Leonardo Costa <leosilvacosta6@gmail.com>
2020-12-28 13:43:06 -0300

    Facial extraction

--- Webcam/AAM.py
+++ Webcam/AAM.py
@@ -1,3 +1,4 @@
+#!/bin/python3
 from imutils.video import VideoStream
 from imutils import face_utils
 import datetime
@@ -6,6 +7,7 @@ import imutils
 import time
 import dlib
 import cv2
+import numpy as np
 
 
 # Argument parsing
@@ -31,17 +33,42 @@ while True:
     frame = imutils.resize(frame, width=400)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    rects = detector(gray, 0)
+    faces = detector(gray, 0)
 
-    for rect in rects:
+    for face in faces:
 
-        shape = predictor(gray, rect)
+        shape = predictor(gray, face)
+
+        landmarks_points = []
+        for n in range(0, 27):
+            x = shape.part(n).x
+            y = shape.part(n).y
+            landmarks_points.append((x, y))
+
         shape = face_utils.shape_to_np(shape)
 
-        for (x, y) in shape:
-            cv2.circle(frame, (x, y), 3, (0, 0, 255), -1)
+        # for i in range(0, len(landmarks_points)-1):
+        # # cv2.circle(frame, (x, y), 2, (0, 0, 255), -1)
+        # if i != 16:
+        # cv2.line(frame, landmarks_points[i],
+        # landmarks_points[i+1], (0, 0, 255), 2)
+        # else:
+        # cv2.line(frame, landmarks_points[i],
+        # landmarks_points[-1], (0, 0, 255), 2)
+        # if i == 0:
+        # cv2.line(frame, landmarks_points[i],
+        # landmarks_points[17], (0, 0, 255), 2)
 
-        cv2.imshow("Frame", frame)
+        remap = np.zeros_like(shape)
+        out_face = np.zeros_like(frame)
+        feature_mask = np.zeros((frame.shape[0], frame.shape[1]))
+
+        remap = cv2.convexHull(shape)
+        cv2.fillConvexPoly(feature_mask, remap[0:27], 1)
+        feature_mask = feature_mask.astype(np.bool)
+        out_face[feature_mask] = frame[feature_mask]
+
+        cv2.imshow("Frame", out_face)
         key = cv2.waitKey(1) & 0xFF
 
         if key == ord("q"):
